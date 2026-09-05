bc5f61b2d2d0caf4696de74c7170081ee2d4665a
shinaskvzr-cell <shinaskvzr@gmail.com>
2026-09-05 13:50:47 +0530

    perf: remove command interface allocation

--- internal/engine/command/command.go
+++ internal/engine/command/command.go
@@ -1,5 +1,21 @@
 package command
 
-type Command interface {
-	isCommand()
+import "velocity/internal/domain/order"
+
+type Kind uint8
+
+const (
+	Submit Kind = iota
+	Cancel
+	Modify
+)
+
+type Command struct {
+	Kind        Kind
+	Order       *order.Order
+	OrderID     int64
+	NewPrice    int64
+	NewQuantity int64
+	Result      chan error
+	Done        chan struct{}
 }
